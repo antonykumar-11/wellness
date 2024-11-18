61626d3f6168aa90a70b6eabc1524bf1bdab2f5e
antonykumar-11 <wellnessveda7@gmail.com>
2024-11-18 13:42:02 +0530

    Updated ItemsHeader.jsx

--- client/src/items/ItemsHeader.jsx
+++ client/src/items/ItemsHeader.jsx
@@ -5,18 +5,6 @@ function InvoiceHeader() {
     <div className="sticky top-0 bg-blue-300 dark:bg-black dark:text-white text-black  hidden xl:block  p-2 z-30">
       <div className="flex  justify-between md:justify-end space-x-6 font-bold">
         <NavLink
-          to="/invoice"
-          className="px-3 py-1 no-underline rounded hover:text-white hover:bg-black dark:hover:bg-gradient-to-r from-green-400 to-blue-500"
-        >
-          create Invoice
-        </NavLink>
-        <NavLink
-          to="/invoice/invoice-preview"
-          className="px-3 py-1 no-underline rounded hover:text-white hover:bg-black dark:hover:bg-gradient-to-r from-green-400 to-blue-500"
-        >
-          invoice previews
-        </NavLink>
-        <NavLink
           to="/invoice/customerManagement"
           className="px-3 py-1 no-underline rounded hover:text-white hover:bg-black dark:hover:bg-gradient-to-r from-green-400 to-blue-500"
         >
