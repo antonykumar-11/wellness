d3fd3163f214ea94398772e24eba4fb23e989f45
antonykumar-11 <wellnessveda7@gmail.com>
2024-11-18 11:55:39 +0530

    Update Tailwind config, index.css, and server environment

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,10 +9,14 @@ module.exports = {
   ],
   theme: {
     screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1400px",
+      xs: "320px",
+      sm: "375px",
+      sml: "500px",
+      md: "667px",
+      mdl: "768px",
+      lg: "960px",
+      lgl: "1024px",
+      xl: "1280px",
     },
     extend: {
       colors: {
@@ -24,6 +28,14 @@ module.exports = {
         whiteColor: "#fff",
         textColor: "#DDD",
         secondaryColor: "red",
+
+        bodyColor: "#212428",
+        lightText: "#c4cfde",
+        boxBg: "linear-gradient(145deg, #1e2024, #23272b)",
+        designColor: "#ff014f",
+      },
+      boxShadow: {
+        shadowOne: "10px 10px 19px #1c1e22, -10px -10px 19px #262a2e",
       },
       keyframes: {
         move: {
@@ -47,6 +59,8 @@ module.exports = {
         poppins: ["Poppins", "sans-serif"],
         londrina: ["Londrina Outline", "sans-serif"],
         quickSand: ["Quicksand", "sans-serif"],
+        bodyFont: ["Poppins", "sans-serif"],
+        titleFont: ["Montserrat", "sans-serif"],
       },
     },
     container: {
